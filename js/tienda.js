cf5afbe1fe4bd811887b4533126ce812581fbfdd
Joaquin Aenishanslins <jo.aenishanslins@duocuc.clgit config --global user.email jo.aenishanslins@duocuc.cl>
2026-09-08 20:07:27 -0300

    mas agregados

---
+++ js/tienda.js
@@ -0,0 +1,10 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'Catan (Juego Base)',
+        categoria: 'Estrategia',
+        precio: 39990,
+        imagen: 'img/catan.jpg',
+        descripcion: 'El clásico juego de mesa de comercio y construcción. Construye carreteras, pueblos y ciudades mientras negocias con otros jugadores para obtener recursos.',
+    }
+]
