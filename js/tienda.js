9948503d96f3e87d98afbb8fadb1abd48c12ba4e
Joaquin Aenishanslins <jo.aenishanslins@duocuc.clgit config --global user.email jo.aenishanslins@duocuc.cl>
2026-09-08 20:10:56 -0300

    mas agregados

--- js/tienda.js
+++ js/tienda.js
@@ -4,7 +4,15 @@ const productos = [
         nombre: 'Catan (Juego Base)',
         categoria: 'Estrategia',
         precio: 39990,
-        imagen: 'img/catan.jpg',
+        imagen: 'img/catan.webp',
         descripcion: 'El clásico juego de mesa de comercio y construcción. Construye carreteras, pueblos y ciudades mientras negocias con otros jugadores para obtener recursos.',
+    },
+    {
+        id: 2,
+        nombre: 'Booster Pack Pokemon TCG',
+        categoria: 'TCG',
+        precio: 4990,
+        imagen: 'img/pokemon.webp',
+        descripcion: 'Un paquete de refuerzo del juego de cartas coleccionables Pokémon. Contiene cartas aleatorias que pueden incluir Pokémon, entrenadores y energías.',
     }
 ]
