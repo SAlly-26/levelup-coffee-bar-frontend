3d409731c14befa45c1b6ea7691bed0247179515
Joaquin Aenishanslins <jo.aenishanslins@duocuc.clgit config --global user.email jo.aenishanslins@duocuc.cl>
2026-09-08 20:29:24 -0300

    mas agregados

--- js/tienda.js
+++ js/tienda.js
@@ -14,5 +14,56 @@ const productos = [
         precio: 4990,
         imagen: 'img/pokemon.webp',
         descripcion: 'Un paquete de refuerzo del juego de cartas coleccionables Pokémon. Contiene cartas aleatorias que pueden incluir Pokémon, entrenadores y energías.',
+    },
+    {
+        id: 3,
+        nombre: 'Set de Dados D&D "Nebulosa"',
+        categoria: 'Rol',
+        precio: 12990,
+        imagen: 'img/dados.webp',
+        descripcion: 'Un conjunto de dados de poliédrico para juegos de rol, con un diseño de nebulosa en colores vibrantes. Incluye dados de 4, 6, 8, 10, 12 y 20 caras.',
+    },
+    {
+        id: 4,
+        nombre: 'Exploding Kittens',
+        categoria: 'Cartas',
+        precio: 19990,
+        imagen: 'img/kittens.webp',
+        descripcion: 'Un juego de cartas donde los gatos explotan. Los jugadores deben evitar los gatos explosivos mientras intentan sobrevivir.',
     }
-]
+];
+
+function mostrarProductos() {
+    const contenedor = document.getElementById('contenedor-productos');
+
+    contenedor.innerHTMML = productos.map(producto => `
+        <div class="col-sm-6 col-lg-3">
+            <div class="card-producto">
+
+                <div class="producto-imagen">
+                    <img src="${producto.imagen}" alt="${prodcuto.nombre}">
+                </div>
+
+                <div class="producto-body">
+                    <span class="producto-bodycat-${producto.categoria.toLowerCase()}">
+                        ${producto.categoria}
+                    </span>
+                    <p class="producto-nombre">${producto.nombre}</p>
+                    <p class="producto-descripcion">${producto.descripcion}</p>
+                    <p class="producto-precio">$${producto.precio.toLocaleString('es-CL')}</p>
+
+                    <div class="d-flex align-items-center gap-2 mt-2">
+                        <div class="control-cantidad">
+                            <button class="btn-cantidad" onclick="cambiarCantidad(${producto.id}, -1)">-</button>
+                            <span class="numero-cantidad" id="cantidad-${producto.id}">1</span>
+                            <button class="btn-cantidad" onclick="cambiarCantidad(${producto.id}, 1)">+</button>
+                        </div>
+                        <button class="btn-agregar" onclick="agregarAlCarrito(${producto.id})">
+                            Añadir al carrito
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `).join('');
+}
